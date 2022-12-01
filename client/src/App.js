06c34a199b349f503b6436f3e4e0f86be0793c36
wilson <wilsonmv90@gmail.com>
2022-11-30 21:50:56 -0500

    second commit

---
+++ client/src/App.js
@@ -0,0 +1,258 @@
+import { Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div id="wrapper">
+      <ul
+        className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+        id="accordionSidebar"
+      >
+        <Link
+          to="/"
+          className="sidebar-brand d-flex align-items-center justify-content-center"
+          href="index.html"
+        >
+          <div className="sidebar-brand-icon rotate-n-15">
+            <i className="fas fa-laugh-wink"></i>
+          </div>
+          <div className="sidebar-brand-text mx-3">Instaya</div>
+        </Link>
+
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            <i class="fas fa-solid fa-user"></i>
+            <span>Login</span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            <i className="fas fa-fw fa-table"></i>
+            <span>Registro</span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/listaPedidos" className="nav-link">
+            <i class="fas fa-solid fa-rocket"></i>
+            <span>Lista de Pedidos</span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/RegistroPedidos" className="nav-link">
+            <i class="fas fa-solid fa-address-book"></i>
+            <span>Registro de Pedidos</span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/actualizacionPedidos" className="nav-link">
+            <i class="fas fa-solid fa-pen-nib"></i>
+            <span>Actualización de Pedidos</span>
+          </Link>
+        </li>
+      </ul>
+
+      <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+          <div className="container-fluid">
+            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 className="h3 mt-5 mb-0 text-gray-800">Dashboard</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xl-3 col-md-6 mb-4">
+              <div className="card border-left-primary shadow h-100 py-2">
+                <div className="card-body">
+                  <div className="row no-gutters align-items-center">
+                    <div className="col mr-2">
+                      <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                        Earnings (Monthly)
+                      </div>
+                      <div className="h5 mb-0 font-weight-bold text-gray-800">
+                        $40,000
+                      </div>
+                    </div>
+                    <div className="col-auto">
+                      <i className="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-md-6 mb-4">
+              <div className="card border-left-success shadow h-100 py-2">
+                <div className="card-body">
+                  <div className="row no-gutters align-items-center">
+                    <div className="col mr-2">
+                      <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
+                        Earnings (Annual)
+                      </div>
+                      <div className="h5 mb-0 font-weight-bold text-gray-800">
+                        $215,000
+                      </div>
+                    </div>
+                    <div className="col-auto">
+                      <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-md-6 mb-4">
+              <div className="card border-left-info shadow h-100 py-2">
+                <div className="card-body">
+                  <div className="row no-gutters align-items-center">
+                    <div className="col mr-2">
+                      <div className="text-xs font-weight-bold text-info text-uppercase mb-1">
+                        Tasks
+                      </div>
+                      <div className="row no-gutters align-items-center">
+                        <div className="col-auto">
+                          <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                            50%
+                          </div>
+                        </div>
+                        <div className="col">
+                          <div className="progress progress-sm mr-2">
+                            <div
+                              className="progress-bar bg-info"
+                              role="progressbar"
+                              style={{ width: "50%" }}
+                              aria-valuenow="50"
+                              aria-valuemin="0"
+                              aria-valuemax="100"
+                            ></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-auto">
+                      <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-md-6 mb-4">
+              <div className="card border-left-warning shadow h-100 py-2">
+                <div className="card-body">
+                  <div className="row no-gutters align-items-center">
+                    <div className="col mr-2">
+                      <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                        Pending Requests
+                      </div>
+                      <div className="h5 mb-0 font-weight-bold text-gray-800">
+                        18
+                      </div>
+                    </div>
+                    <div className="col-auto">
+                      <i className="fas fa-comments fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-6 mb-4">
+              <div className="card shadow mb-4">
+                <div className="card-header py-3">
+                  <h6 className="m-0 font-weight-bold text-primary">
+                    Projects
+                  </h6>
+                </div>
+                <div className="card-body">
+                  <h4 className="small font-weight-bold">
+                    Server Migration <span className="float-right">20%</span>
+                  </h4>
+                  <div className="progress mb-4">
+                    <div
+                      className="progress-bar bg-danger"
+                      role="progressbar"
+                      style={{ width: "20%" }}
+                      aria-valuenow="20"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <h4 className="small font-weight-bold">
+                    Sales Tracking <span className="float-right">40%</span>
+                  </h4>
+                  <div className="progress mb-4">
+                    <div
+                      className="progress-bar bg-warning"
+                      role="progressbar"
+                      style={{ width: "40%" }}
+                      aria-valuenow="40"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <h4 className="small font-weight-bold">
+                    Customer Database <span className="float-right">60%</span>
+                  </h4>
+                  <div className="progress mb-4">
+                    <div
+                      className="progress-bar"
+                      role="progressbar"
+                      style={{ width: "60%" }}
+                      aria-valuenow="60"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <h4 className="small font-weight-bold">
+                    Payout Details <span className="float-right">80%</span>
+                  </h4>
+                  <div className="progress mb-4">
+                    <div
+                      className="progress-bar bg-info"
+                      role="progressbar"
+                      style={{ width: "80%" }}
+                      aria-valuenow="80"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <h4 className="small font-weight-bold">
+                    Account Setup <span className="float-right">Complete!</span>
+                  </h4>
+                  <div className="progress">
+                    <div
+                      className="progress-bar bg-success"
+                      role="progressbar"
+                      style={{ width: "100%" }}
+                      aria-valuenow="100"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <footer className="sticky-footer bg-white">
+          <div className="container my-auto">
+            <div className="copyright text-center my-auto">
+              <span>Copyright &copy; Will Maldonado MisionTic 2022</span>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Componentes/OrderList.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const OrderList = () => {
+  return (
+    <div class="container">
+      <Link to="/">
+        <button type="button" class="btn btn-primary my-4">
+          Regresar
+        </button>
+      </Link>
+
+      <div class="d-flex justify-content-center"></div>
+      <div className="card shadow mb-4">
+        <div className="card-header py-3 d-flex justify-content-between ">
+          <h2 className="m-0 font-weight-bold text-primary">
+            Lista de Ordenes
+          </h2>
+          <Link to="/RegistroPedidos">
+            <button type="button" class="btn btn-primary mt-2">
+              Crear Orden
+            </button>
+          </Link>
+        </div>
+
+        <div className="card-body">
+          <div className="table-responsive">
+            <table
+              className="table table-bordered"
+              id="dataTable"
+              width="100%"
+              cellspacing="0"
+            >
+              <thead>
+                <tr>
+                  <th># Servicio</th>
+                  <th>Fecha</th>
+                  <th>Ciudad Entrega</th>
+                  <th>Dirrección Entrega</th>
+                  <th>Estado</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>2011/04/25</td>
+                  <td>Edinburgh</td>
+                  <td>Calle 4 # 23-122</td>
+                  <td>Guardado</td>
+                  <td><button className="btn btn-success"/*  onClick={()=>seleccionarPais(elemento, 'Editar')} */><i class="fas fa-solid fa-pen"></i>  Editar </button> {"  "} 
+              <button className="btn btn-danger"/*  onClick={()=>seleccionarPais(elemento, 'Eliminar')} */><i class="fas fa-solid fa-trash"></i>  Eliminar</button></td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>2011/07/25</td>
+                  <td>Tokyo</td>
+                  <td>Calle 7# 46-64</td>
+                  <td>Cancelado</td>
+                  <td><button className="btn btn-success"/*  onClick={()=>seleccionarPais(elemento, 'Editar')} */><i class="fas fa-solid fa-pen"></i>  Editar </button> {"  "} 
+              <button className="btn btn-danger"/*  onClick={()=>seleccionarPais(elemento, 'Eliminar')} */><i class="fas fa-solid fa-trash"></i>  Eliminar</button></td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>2009/01/12</td>
+                  <td>San Francisco</td>
+                  <td>Cra 30 # 25-40</td>
+                  <td>Cumplido</td>
+                  <td><button className="btn btn-success"/*  onClick={()=>seleccionarPais(elemento, 'Editar')} */><i class="fas fa-solid fa-pen"></i>  Editar </button> {"  "} 
+              <button className="btn btn-danger"/*  onClick={()=>seleccionarPais(elemento, 'Eliminar')} */><i class="fas fa-solid fa-trash"></i>  Eliminar</button></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
